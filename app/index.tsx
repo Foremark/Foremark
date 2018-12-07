3050ae8d6f4e32ba6369a2375d1df57b902703cd
yvt <i@yvt.jp>
2018-12-08 00:32:03 +0900

    Add a startup code including polyfills for IE11

--- app/index.tsx
+++ app/index.tsx
@@ -1,1 +1,8 @@
+if ('あ'.charCodeAt(0) !== 0x3042) {
+    throw new Error('The application was loaded with a wrong encoding.');
+}
+
+// Polyfill mainly for IE11
+require('./ie11');
+
 console.log("yeehaw");
