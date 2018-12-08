c99f201bde90f3c106d5e5e9436e994e18774478
yvt <i@yvt.jp>
2018-12-09 01:24:30 +0900

    mfview: Support equations

--- app/mfview.ts
+++ app/mfview.ts
@@ -1,4 +1,5 @@
 import {highlightAuto} from '../lib/highlight';
+import * as katex from 'katex';
 
 /**
  * Transforms Markfront XML for viewing.
@@ -16,12 +17,43 @@ export function prepareMarkfrontForViewing(node: Element): void {
     }
 }
 
+const katexInlineOptions = {
+    // These macros are defined by Markdeep
+    macros: {
+        '\\n': '\\hat{n}',
+        '\\w': '{\\hat{\\omega}}',
+        '\\wi': '{\\w_\\mathrm{i}}',
+        '\\wo': '{\\w_\\mathrm{o}}',
+        '\\wh': '{\\w_\\mathrm{h}}',
+        '\\Li': '{L_\\mathrm{i}}',
+        '\\Lo': '{L_\\mathrm{o}}',
+        '\\Le': '{L_\\mathrm{e}}',
+        '\\Lr': '{L_\\mathrm{r}}',
+        '\\Lt': '{L_\\mathrm{t}}',
+        '\\O': '{\\mathrm{O}}',
+        '\\degrees': '{{^{\\large\\circ}}}',
+        '\\T': '{\\mathsf{T}}',
+        '\\mathset': '[1]{\\mathbb{#1}}',
+        '\\Real': '{\\mathset{R}}',
+        '\\Integer': '{\\mathset{Z}}',
+        '\\Boolean': '{\\mathset{B}}',
+        '\\Complex': '{\\mathset{C}}',
+    },
+    throwOnError: false,
+} as katex.KatexOptions;
+const katexDisplayOptions: katex.KatexOptions = {
+    displayMode: true,
+    ... katexInlineOptions,
+};
+
+require('../lib/katex.less');
+
 const HANDLERS: { [tagName: string]: (node: Element) => void } & Object = {
     'mf-eq': node => {
-        // TODO
+        node.innerHTML = katex.renderToString(node.textContent || '', katexInlineOptions);
     },
     'mf-eq-display': node => {
-        // TODO
+        node.innerHTML = katex.renderToString(node.textContent || '', katexDisplayOptions);
     },
     'mf-code': node => {
         const type = (node.getAttribute('type') || '').split(' ');
