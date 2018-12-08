b00efcb6295bada76b8e81dd28a7e99c3a829cc5
yvt <i@yvt.jp>
2018-12-08 23:47:37 +0900

    `transformHtmlWith` now only accepts `Element`

--- app/dom.ts
+++ app/dom.ts
@@ -33,71 +33,53 @@ const PLACEHOLDER_REGEX = /<[-_a-zA-Z0-9]+ id="([0-9]+)" \/>/;
  * `recursionFilter`.
  */
 export function transformHtmlWith(
-    node: Node,
+    node: Element,
     tx: (s: string) => string,
     recursionFilter?: (e: Element) => boolean,
 ) {
-    switch (node.nodeType) {
-        case NodeType.TEXT_NODE:
-        case NodeType.CDATA_SECTION_NODE:
-            node.textContent = decodeHTML(tx(
-                node.textContent!
-                    .replace(/&/g, '&amp;')
-                    .replace(/</g, '&lt;')
-                    .replace(/>/g, '&gt;')
-            ));
-            break;
-        case NodeType.ELEMENT_NODE:
-            if (!(node instanceof Element)) {
-                throw new Error();
+    // Get the inner HTML
+    const placeholders = new Map<string, Node>();
+    let html = '';
+    for (let n: Node | null = node.firstChild; n; n = n.nextSibling) {
+        if (n instanceof Text) {
+            html += n.textContent!
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+        } else if (n instanceof Element) {
+            if (recursionFilter && recursionFilter(n)) {
+                transformHtmlWith(n, tx, recursionFilter);
             }
 
-            // Get the inner HTML
-            const placeholders = new Map<string, Node>();
-            let html = '';
-            for (let n: Node | null = node.firstChild; n; n = n.nextSibling) {
-                if (n instanceof Text) {
-                    html += n.textContent!
-                        .replace(/&/g, '&amp;')
-                        .replace(/</g, '&lt;')
-                        .replace(/>/g, '&gt;');
-                } else if (n instanceof Element) {
-                    if (recursionFilter && recursionFilter(n)) {
-                        transformHtmlWith(n, tx, recursionFilter);
-                    }
+            let placeholderId = String(nextPlaceholderId++);
+            placeholders.set(placeholderId, n);
+            html += placeholderHtmlWithId(n.tagName, placeholderId);
+        } else {
+            let placeholderId = String(nextPlaceholderId++);
+            placeholders.set(placeholderId, n);
+            html += placeholderHtmlWithId('mf-ph', placeholderId);
+        }
+    }
 
-                    let placeholderId = String(nextPlaceholderId++);
-                    placeholders.set(placeholderId, n);
-                    html += placeholderHtmlWithId(n.tagName, placeholderId);
-                } else {
-                    let placeholderId = String(nextPlaceholderId++);
-                    placeholders.set(placeholderId, n);
-                    html += placeholderHtmlWithId('mf-ph', placeholderId);
-                }
-            }
+    const orig = html;
+    html = tx(html);
 
-            const orig = html;
-            html = tx(html);
+    if (orig === html) {
+        return;
+    }
 
-            if (orig === html) {
-                return;
-            }
+    // Replace the old contents
+    node.innerHTML = '';
 
-            // Replace the old contents
-            node.innerHTML = '';
+    const parts = html.split(PLACEHOLDER_REGEX);
 
-            const parts = html.split(PLACEHOLDER_REGEX);
+    if (parts[0] !== '') {
+        node.innerHTML = parts[0];
+    }
 
-            if (parts[0] !== '') {
-                node.innerHTML = parts[0];
-            }
-
-            for (let i = 1; i < parts.length; i += 2) {
-                let placeholderId = parts[i];
-                node.appendChild(placeholders.get(placeholderId)!);
-                node.insertAdjacentHTML('beforeend', parts[i + 1]);
-            }
-
-            break;
+    for (let i = 1; i < parts.length; i += 2) {
+        let placeholderId = parts[i];
+        node.appendChild(placeholders.get(placeholderId)!);
+        node.insertAdjacentHTML('beforeend', parts[i + 1]);
     }
 }
