Foremark/Foremark

commit 0463d8e22bf71751ddd7df77f9bdee208c076bb4

Permit `<pre>` as the document root

app/browser-stage1.tsx
@@ -10,7 +10,7 @@ import {processSitemap} from './view/sitemap';
 
 export async function browserMain(): Promise<void> {
     // Load the input from the current document
-    let inputNode = document.querySelector(`${TagNames.Document}, ${TagNames.Text}`);
+    let inputNode = document.querySelector(`${TagNames.Document},${TagNames.Text},pre`);
     if (inputNode == null) {
         inputNode = document.createElement(TagNames.Document);
         inputNode.innerHTML = `<${TagNames.Error}>
@@ -25,8 +25,16 @@ export async function browserMain(): Promise<void> {
     const [sitemap, sitemapErrors] = processSitemap(
         viewerConfig.sitemap, viewerConfig.sitemapDocumentRoot);
 
-    // `<mf-text>` is a shorthand syntax for `<mf><mf-text>...</mf-text></mf>`
-    if (inputNode.tagName.toLowerCase() === TagNames.Text) {
+    // `<mf-text>` and `<pre>` are a shorthand syntax for `<mf><mf-text>...</mf-text></mf>`
+    if (/^pre$/i.test(inputNode.tagName)) {
+        const mf = document.createElement(TagNames.Document);
+        const mfText = document.createElement(TagNames.Text);
+        while (inputNode.firstChild) {
+            mfText.appendChild(inputNode.firstChild);
+        }
+        mf.appendChild(mfText);
+        inputNode = mf;
+    } else if (inputNode.tagName.toLowerCase() === TagNames.Text) {
         const mf = document.createElement(TagNames.Document);
         mf.appendChild(inputNode);
         inputNode = mf;

examples/basic.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><body><mf-text>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><pre>
 
                         **Foremark Example**
 
@@ -44,6 +44,5 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     ::: '---------'  '---------'
 ]]>
 
-</mf-text> <!-- Foremark footer -->
-<script src="../browser/foremark.js" data-rel="foremark" async="async" />
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+</pre> <!-- Foremark footer -->
+<script src="../browser/foremark.js" data-rel="foremark" async="async" /></html>

examples/best-friends-until-the-end-of-time-ja.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja"><pre><![CDATA[
 
                                     **ずっと友達**
 [Best Friends Until the End of Time](http://mlp.wikia.com/wiki/Best_Friends_Until_the_End_of_Time)
@@ -98,6 +98,5 @@
 > as long as (we) act as one.
 
 
-]]></mf-text> <!-- Foremark footer -->
-<script src="../browser/foremark.js" data-rel="foremark" async="async" />
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+]]></pre> <!-- Foremark footer -->
+<script src="../browser/foremark.js" data-rel="foremark" async="async" /></html>

examples/best-friends-until-the-end-of-time-jbo.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="jbo"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="jbo"><pre><![CDATA[
 
                             **ze'eba simxu pendo**
 zoi .gy. [Best Friends Until the End of Time](http://mlp.wikia.com/wiki/Best_Friends_Until_the_End_of_Time) .gy.
@@ -104,6 +104,5 @@ tu'uli'u
 
 mu'o
 
-]]></mf-text> <!-- Foremark footer -->
-<script src="../browser/foremark.js" data-rel="foremark" async="async" />
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+]]></pre> <!-- Foremark footer -->
+<script src="../browser/foremark.js" data-rel="foremark" async="async" /></html>

examples/book.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><pre><![CDATA[
 
                   **The Foremark Book**
 
@@ -8,7 +8,7 @@
 
 [the reference manual]: reference.mf.xhtml
 
-]]></mf-text> <!-- Foremark footer -->
+]]></pre> <!-- Foremark footer -->
 <script src="../browser/foremark.js" data-rel="foremark" async="async" />
 <script src="global.js" data-rel="foremark-config" async="async" />
 
@@ -33,4 +33,4 @@ mf-code[type="XML converted"]:after {
 }
 </style>
 
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+</html>

examples/markdeep-features.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><pre><![CDATA[
 
                       **Markdeep Feature Demo**
                            Morgan McGuire
@@ -1902,6 +1902,5 @@ that appear in the document, you cannot nest a code tag inside of another code t
 likewise for pre tags.  Fortunately, it is pretty hard to imagine a case where you would want
 nested code tags.
 
-]]></mf-text> <!-- Foremark footer -->
-<script src="../browser/foremark.js" data-rel="foremark" async="async" />
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+]]></pre> <!-- Foremark footer -->
+<script src="../browser/foremark.js" data-rel="foremark" async="async" /></html>

examples/media.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><pre><![CDATA[
 
                         **Media element test**
 
@@ -53,7 +53,6 @@ Inline frame
 ![!][](https://sketchfab.com/models/c461e00264ab457e964a3755b81b5f8f):
     Sketchfab
 
-]]></mf-text> <!-- Foremark footer -->
+]]></pre> <!-- Foremark footer -->
 <script src="../browser/foremark.js" data-rel="foremark" async="async" />
-<script src="global.js" data-rel="foremark-config" async="async" />
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+<script src="global.js" data-rel="foremark-config" async="async" /></html>

examples/reference.mf.xhtml
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><pre><![CDATA[
 
                   **Foremark Reference Manual**
 
@@ -1011,7 +1011,7 @@ Unlike sidenotes, citations are always rendered where they are defined (so that 
 A Foremark document is an XHTML file that consolidates Foremark Text/XML markups and a script tag for loading the viewer application.
 
 ~~~~~~~~~~~~ XML
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><body><mf-text><![CDATA[
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><pre><![CDATA[
 
                              **Document title**
 
@@ -1020,14 +1020,17 @@ tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat.
 
-]]]><![CDATA[]></mf-text> <!-- Foremark footer -->
-<script src="../browser/foremark.js" data-rel="foremark" async="async" />
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+]]]><![CDATA[]></pre> <!-- Foremark footer -->
+<script src="../browser/foremark.js" data-rel="foremark" async="async" /></html>
 ~~~~~~~~~~~~
 
-- `html > head > mf` or `html > head > mf-text` represent the content root.
-  If `mf-text` is the content root, it will be wrapped by `<mf>` and all attributes are moved to the `<mf>`.
-- `html > head > script` with a `data-rel="foremark"` attribute instructs the web browser to load the viewer application. The viewer application locates this tag and uses its `src` attribute as a base path for loading other assets.
+- A document includes one of `html > mf`, `html > mf-text`, and `html > pre` which represent the content root.
+  If `mf-text` is the content root, it will be wrapped by `<mf>` and all attributes are moved to the `<mf>`. `pre` is converted to `mf-text` first.
+- `html > script` with a `data-rel="foremark"` attribute instructs the web browser to load the viewer application. The viewer application locates this tag and uses its `src` attribute as a base path for loading other assets.
+
+!!! Note: Rationale
+
+    Using `pre` makes it possible for text browsers (such as Lynx) to display Foremark documents to some extent.
 
 !!! Warning
 
@@ -1265,7 +1268,7 @@ expandMfText(mfText);
 
 [the `jsdom` library]: https://www.npmjs.com/package/jsdom
 
-]]></mf-text> <!-- Foremark footer -->
+]]></pre> <!-- Foremark footer -->
 <script src="../browser/foremark.js" data-rel="foremark" async="async" />
 <script src="global.js" data-rel="foremark-config" async="async" />
 
@@ -1292,4 +1295,4 @@ mf-code[type="XML converted"]:after {
 }
 </style>
 
-<style>mf-text{white-space:pre-wrap;font-family:monospace}</style></body></html>
+</html>

commit f62c2186fa93ddbe4531034085ae462a9687b66f

Fix the doctest fixture to accommodate the change in the root element
This commit fixes a bug in the test fixture introduced in
`0463d8e22bf71751ddd7df77f9bdee208c076bb4`.

test/mftext.js
@@ -59,7 +59,21 @@ function scanDoctest(doctext) {
     });
 
     setWorkingDom(dom.window);
-    expandMfText(dom.window.document.body);
+
+    // Replace top-level `pre` with `mf-text`
+    const html = dom.window.document.querySelector('html');
+    let inputNode = dom.window.document.querySelector('mf-text, pre, mf');
+    {
+        if (inputNode.tagName === 'pre') {
+            const mfText = dom.window.document.createElement('mf-text');
+            while (inputNode.firstChild) {
+                mfText.appendChild(inputNode.firstChild);
+            }
+            html.appendChild(mfText);
+        }
+    }
+
+    expandMfText(html);
 
     const headings = [{
         name: 'Top level',
@@ -67,7 +81,7 @@ function scanDoctest(doctext) {
     }];
     const headingCounter = [0];
 
-    forEachNodePreorder(dom.window.document.body, node => {
+    forEachNodePreorder(html, node => {
         if (node.nodeType !== 1) {
             return;
         }
@@ -84,7 +98,7 @@ function scanDoctest(doctext) {
         } else if (node.tagName === 'mf-codeblock') {
             const code = node.getElementsByTagName('mf-code');
             if (
-                code.length !== 2 || 
+                code.length !== 2 ||
                 code[1].getAttribute('type') !== 'XML converted'
             ) {
                 return false;